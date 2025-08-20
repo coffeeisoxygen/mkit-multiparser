91b2f8b9d16eba41431b53c262a8ddf6bc90231a
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-20 20:33:51 +0700

    docs: update docstring for test_settings fixture
    Enhance the documentation for the test_settings fixture to provide clearer information about its purpose and return type for the testing environment.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,4 +6,12 @@ from app.env_settings import get_settings
 
 @pytest.fixture(scope="session")
 def test_settings():
+    """
+    Test settings fixture for the testing environment.
+
+    This fixture provides the application settings for the testing environment.
+
+    Returns:
+        _type_: The application settings for the testing environment.
+    """
     return get_settings(_env_file=".env.test")
