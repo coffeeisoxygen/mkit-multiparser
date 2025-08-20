c86c3d32f1cee038f90b32768c713680fe760bdd
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-20 21:00:02 +0700

    refactor(logging): improve emit method for clarity
    refactor the emit method in InterceptHandler to enhance readability and maintainability. The changes include better handling of log levels and clearer frame inspection for logging context.

--- app/config/logging.py
+++ app/config/logging.py
@@ -1,13 +1,27 @@
+import inspect
 import logging
 
 from loguru import logger
 
 
 class InterceptHandler(logging.Handler):
-    def emit(self, record: logging.LogRecord) -> None:  # pragma: no cover
-        logger_opt = logger.opt(depth=7, exception=record.exc_info)
-        logger_opt.log(record.levelname, record.getMessage())
+    def emit(self, record: logging.LogRecord) -> None:
+        # Get corresponding Loguru level if it exists.
+        level: str | int
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message.
+        frame, depth = inspect.currentframe(), 0
+        while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
 
 
-logging.getLogger().addHandler(InterceptHandler())
-logging.getLogger().setLevel(logging.DEBUG)
+logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
