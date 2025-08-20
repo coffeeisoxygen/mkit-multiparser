20f842990f957c744a9f6ce7967d99e0ee669443
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-20 22:05:01 +0700

    refactor(logging): remove redundant logger configuration

--- app/config/mlogg.py
+++ app/config/mlogg.py
@@ -1,6 +1,5 @@
 import inspect
 import logging
-import sys
 from pathlib import Path
 
 from loguru import logger
@@ -29,16 +28,6 @@ class InterceptHandler(logging.Handler):
 
 logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
 
-logger.remove()
-logger.add(
-    sys.stdout,
-    format="<level>{level: <8}</level>| <red>{name}</red>:<bold>{function}</bold>:<magenta>{line}</magenta> |  <level>{message}</level> | <l>{extra}</>",
-    level="INFO",
-    enqueue=True,
-    colorize=True,
-    diagnose=True,
-    backtrace=True,
-)
 
 # NOTE: make sure harus terakhir di panggil karena ini akan mereset default, config, jadi jika mau custom, harus di atas
 logyamlpath = Path(__file__).parent.parent.parent / "logging.yaml"
