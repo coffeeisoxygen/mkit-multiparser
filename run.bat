22feaa36527a0917cfb238e0c73c8efeb5662c7e
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-21 07:03:35 +0700

    feat(logging): update log format and environment variable
    Enhance logging configuration by updating the log format for better readability and specifying the environment variable for application context.

--- run.bat
+++ run.bat
@@ -1,1 +1,11 @@
-REM $env:APP_ENV="DEVELOPMENT"
+@echo off
+
+
+REM Activate the virtual environment
+call .venv\Scripts\activate
+set APP_ENV=DEVELOPMENT
+REM Run Uvicorn with custom log config and log level
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload  --log-level=info
+
+
+pause
