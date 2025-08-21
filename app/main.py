3e56a5755f222edf6b4ebba6953ac42dc2e9c78d
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-21 09:01:15 +0700

    feat(logging): add main entry point for application

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 # Import logging setup first!
+import uvicorn
 from fastapi import FastAPI
 from loguru import logger
 
@@ -9,3 +10,16 @@ logger.info("User paid with card 4532-1234-5678-9012")
 logger.info("Contact email: john.doe@example.com")
 settings = get_settings()
 app = FastAPI()
+
+
+if __name__ == "__main__":
+    logger.info("Starting application...")
+    logger.info("Running application with Uvicorn...")
+    uvicorn.run(
+        app="app:main",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        log_config=None,
+        log_level=None,
+    )
