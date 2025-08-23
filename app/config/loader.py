d784a6124ece53acd59cc547f34f91dbcee9ce79
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-23 11:34:22 +0700

    fix(loader): correct typo in docstring and log level
    update the argument name in the docstring for clarity
    and change logger level from info to trace for more detailed logging

--- app/config/loader.py
+++ app/config/loader.py
@@ -11,11 +11,10 @@ from app.config.cfg_core import DEFAULT_ENV_FILE, Settings
 def get_settings(_env_file: str | Path | None = None) -> Settings:
     """Prioritas:.
 
-    1. Argumen _env_filer
-
+    1. Argumen _env_file
     2. ENV_FILE dari environment variable
     3. DEFAULT_ENV_FILE (.env)
     """
     env_file = _env_file or os.getenv("ENV_FILE", DEFAULT_ENV_FILE)
-    logger.info(f"Loading settings from {env_file}")
+    logger.trace(f"Loading settings from {env_file}")
     return Settings(_env_file=env_file, _env_file_encoding="utf-8")  # type: ignore
